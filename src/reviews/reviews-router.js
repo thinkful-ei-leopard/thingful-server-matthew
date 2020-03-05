2edb653e2e8a8e7c15919db6c999076f66d08ddb
Matthew Wagaman <averaqedev@gmail.com>
2020-03-04 16:31:00 -0800

    added basic auth

--- src/reviews/reviews-router.js
+++ src/reviews/reviews-router.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const path = require('path');
 const ReviewsService = require('./reviews-service');
+const { requireAuth } = require('../middleware/basic-auth');
 
 const reviewsRouter = express.Router();
 const jsonBodyParser = express.json();
 
 reviewsRouter
   .route('/')
-  .post(jsonBodyParser, (req, res, next) => {
-    const { thing_id, rating, text, user_id } = req.body;
-    const newReview = { thing_id, rating, text, user_id };
+  .post(requireAuth, jsonBodyParser, (req, res, next) => {
+    const { thing_id, rating, text } = req.body;
+    const newReview = { thing_id, rating, text };
 
     for (const [key, value] of Object.entries(newReview))
       // eslint-disable-next-line eqeqeq
@@ -17,6 +18,8 @@ reviewsRouter
         return res.status(400).json({
           error: `Missing '${key}' in request body`
         });
+
+    newReview.user_id = req.user_id;
 
     ReviewsService.insertReview(
       req.app.get('db'),
