ad53b8fecc704303be08d59212e00427fe97c362
Matthew Wagaman <averaqedev@gmail.com>
2020-03-06 13:43:06 -0800

    added refresh endpoint

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const AuthService = require('./auth-service');
+const { requireAuth } = require('../middleware/jwt-auth');
 
 const authRouter = express.Router();
 const jsonParser = express.json();
@@ -42,5 +43,13 @@ authRouter
       })
       .catch(next);
   });
+
+authRouter.post('/refresh', requireAuth, (req, res) => {
+  const sub = req.user.user_name;
+  const payload = { user_id: req.user.id };
+  res.send({
+    authToken: AuthService.createJwt(sub, payload)
+  });
+});
 
 module.exports = authRouter;
